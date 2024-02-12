ebf972ade75ed6df3f3003de63e3154caba9f34f
ds797 <ds797@duck.com>
2024-02-12 14:09:34 -0500

    Fix icon transition (Svelte 4 defaults to 'local')

--- src/lib/components/Category.svelte
+++ src/lib/components/Category.svelte
@@ -90,11 +90,11 @@
 			<div class="icon">
 				{ #if category.overflow?.category }
 					{ #if category.spend }
-						<svg xmlns="http://www.w3.org/2000/svg" class="underflow" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" transition:scale>
+						<svg xmlns="http://www.w3.org/2000/svg" class="underflow" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" transition:scale|global>
 							<path stroke-linecap="round" stroke-linejoin="round" d="m9 9 6-6m0 0 6 6m-6-6v12a6 6 0 0 1-12 0v-3" />
 						</svg>
 					{ :else }
-						<svg xmlns="http://www.w3.org/2000/svg" class="overflow" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" transition:scale>
+						<svg xmlns="http://www.w3.org/2000/svg" class="overflow" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" transition:scale|global>
 							<path stroke-linecap="round" stroke-linejoin="round" d="m15 15-6 6m0 0-6-6m6 6V9a6 6 0 0 1 12 0v3" />
 						</svg>
 					{ /if }
