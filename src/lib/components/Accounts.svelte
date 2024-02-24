307aec8b39132eb12933f05487d894d3b3012ed9
ds797 <ds797@duck.com>
2024-02-24 15:02:28 -0500

    Add fallback message for Accounts

--- src/lib/components/Accounts.svelte
+++ src/lib/components/Accounts.svelte
@@ -1,15 +1,19 @@
 <script>
 	import { links } from '$lib/stores/user'
+	import { route } from '$lib/stores/ui'
 	import Account from '$lib/components/Account.svelte'
 </script>
 
 <main>
-	{ #each $links.links as link }
-		{ #if link.institution }
-			{ #each link.accounts as account }
-				<Account {link} {account} />
-			{ /each }
-		{ /if }
+	{ #each $links.links.filter(l => l.institution) as link }
+		{ #each link.accounts as account }
+			<Account {link} {account} />
+		{ /each }
+	{ :else }
+		<div />
+		<h3>You haven't linked any banks!</h3>
+		<div />
+		<button class="fill" on:click={() => $route.current = $route.selectAccounts}>Link Institution</button>
 	{ /each }
 </main>
 
@@ -20,5 +24,9 @@
 		box-sizing: border-box;
 		padding: 1rem;
 		gap: 0.5rem;
+	}
+
+	button {
+		padding: 0.5rem;
 	}
 </style>
