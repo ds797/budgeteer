e7343259c1891f6ad16f8204195bd49dd7d1ee52
ds797 <ds797@duck.com>
2024-02-27 16:01:56 -0500

    Add server-side payment security

--- supabase/functions/links/index.ts
+++ supabase/functions/links/index.ts
@@ -1,6 +1,7 @@
 import { cors } from '../_shared/cors.ts'
 import { plaid, get, set, remove, refresh, create } from '../_shared/plaid.ts'
 import { user } from '../_shared/user.ts'
+import { service } from '../_shared/service.ts'
 import { respond, err } from '../_shared/response.ts'
 
 // Cases: get, set, remove, refresh
@@ -8,7 +9,15 @@ Deno.serve(async (req: Request) => {
 	if (req.method === 'OPTIONS') return new Response('ok', { headers: cors })
 
 	const user_id = await user(req)
+	// No user?
 	if (!user_id) return err('Unauthorized', 400)
+	// Paying?
+	let paid = false
+	if (user_id) {
+		const { data } = await service.from('payments').select('*')
+		paid = data.filter((payment: any) => payment.user_id === user_id).length ? true : false
+	}
+	if (!paid) return err('Unauthorized', 400)
 
 	const { type } = await req.json()
 
