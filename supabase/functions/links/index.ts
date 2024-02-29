4ffa7c7018b49a261fc5a827bc5eccc2d8e29e2d
ds797 <ds797@duck.com>
2024-02-29 04:43:46 -0500

    Fetch investments in Edge functions

--- supabase/functions/links/index.ts
+++ supabase/functions/links/index.ts
@@ -4,6 +4,43 @@ import { user } from '../_shared/user.ts'
 import { service } from '../_shared/service.ts'
 import { respond, err } from '../_shared/response.ts'
 
+const format = (d: Date) => `${d.getFullYear()}-${(d.getMonth() + 1).toString().padStart(2, '0')}-${(d.getDate()).toString().padStart(2, '0')}`
+
+const investments = async (user_id: string, ids: any[]) => {
+	const links = await get(user_id, (l: any) => ids.find((m: any) => m === l.id), { secrets: true })
+	const holdings: any = []
+	const transactions: any = []
+	const start: Date = new Date()
+	start.setDate(-7)
+	const end: Date = new Date()
+	for (const { id, access_token, institution } of links) {
+		if (!institution) continue
+
+		try {
+			const { data: hs } = await plaid.investmentsHoldingsGet({ access_token })
+			const { data: ts } = await plaid.investmentsTransactionsGet({
+				access_token,
+				start_date: format(start),
+				end_date: format(end)
+			})
+			holdings.push({ id, holdings: hs.holdings, securities: hs.securities })
+			transactions.push(ts.investment_transactions)
+		} catch (error) {
+			if (error.response)
+				if (error.response.data.error_type === 'ITEM_ERROR') continue
+				else throw new Error(error.response.data.error_message)
+			throw new Error(error.message)
+		}
+	}
+
+	const { error } = await service.from('investments').upsert(holdings.map((h: any) => {
+		return { ...h, user_id, transactions: transactions.filter((t: any) => t.account_id === h.account_id).flat() }
+	}))
+	if (error) throw new Error(error.message)
+
+	return { ...holdings, transactions: transactions.flat() }
+}
+
 // Cases: get, set, remove, refresh
 Deno.serve(async (req: Request) => {
 	if (req.method === 'OPTIONS') return new Response('ok', { headers: cors })
@@ -24,7 +61,9 @@ Deno.serve(async (req: Request) => {
 	if (type.get) {
 		const links = await get(user_id, () => true, { logos: true })
 
-		return respond(links)
+		const is = await investments(user_id, links.map((l: any) => l.id))
+
+		return respond({ links, investments: is })
 	} else if (type.set) {
 		const links = type.set
 
@@ -40,7 +79,7 @@ Deno.serve(async (req: Request) => {
 	} else if (type.refresh) {
 		const links = await refresh(user_id, () => true)
 
-		return respond(links)
+		return respond({ links, investments: await investments(user_id, links.map((l: any) => l.id)) })
 	} else if (type.create) {
 		const public_token = type.create
 
@@ -55,7 +94,7 @@ Deno.serve(async (req: Request) => {
 			user: { client_user_id: user_id },
 			client_name: 'Budgeteer',
 			language: 'en',
-			products: ['auth', 'transactions'],
+			products: ['auth', 'transactions', 'investments'],
 			country_codes: ['US'],
 			webhook: 'https://www.example.com/webhook',
 		}
