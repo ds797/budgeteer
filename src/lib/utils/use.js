385e6753a2a6ba0ae01b19ab47813ad808beb1a0
ds797 <ds797@duck.com>
2024-03-03 16:23:20 -0800

    Add use:scroll (be17376)

--- src/lib/utils/use.js
+++ src/lib/utils/use.js
@@ -1,12 +1,33 @@
 export const outside = node => {
-  const click = e => {
+	const click = e => {
 		if (e.target === node && !e.defaultPrevented)
 			node.dispatchEvent(new CustomEvent('child', { detail: e }))
-    if (node && !node.contains(e.target) && !e.defaultPrevented)
-      node.dispatchEvent(new CustomEvent('outside', { detail: e }))
-  }
+		if (node && !node.contains(e.target) && !e.defaultPrevented)
+			node.dispatchEvent(new CustomEvent('outside', { detail: e }))
+	}
 
 	document.addEventListener('click', click, true)
-  
-  return { destroy: () => document.removeEventListener('click', click, true) }
+	
+	return { destroy: () => document.removeEventListener('click', click, true) }
+}
+
+export const scroll = node => {
+	const calc = e => {
+		let x = 0
+		let y = 0
+
+		let parent = e.target
+
+		while (parent) {
+			x += parent.scrollLeft ?? 0
+			y += parent.scrollTop ?? 0
+			parent = parent.parentNode
+		}
+
+		node.dispatchEvent(new CustomEvent('change', { detail: { x, y } }))
+	}
+
+	document.addEventListener('scroll', calc, true)
+
+	return { destroy: () => document.removeEventListener('scroll', calc, true) }
 }
