28e44f5b1752cc5996d8a357a0aed2b9ee915116
ds797 <ds797@duck.com>
2024-03-09 14:21:04 -0800

    Set gradient coordinates to absolute positioning

--- src/lib/components/chart/Gradient.svelte
+++ src/lib/components/chart/Gradient.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <defs>
-	<linearGradient id={name} x1='0' y1='0' x2='0' y2='1'>
-		<stop offset='0' stop-color='var(--text-bad)' stop-opacity='1' />
-		<stop offset='0.5' stop-color='var(--text-okay)' stop-opacity='1' />
-		<stop offset='1' stop-color='var(--text-good)' stop-opacity='1' />
+	<linearGradient id={name} x1='0' y1='0' x2='0' y2='1' gradientUnits='userSpaceOnUse'>
+		<stop offset='0.2' stop-color='var(--text-bad)' stop-opacity='1' />
+		<stop offset='0.45' stop-color='var(--text-okay)' stop-opacity='1' />
+		<stop offset='0.55' stop-color='var(--text-good)' stop-opacity='1' />
 	</linearGradient>
 </defs>
