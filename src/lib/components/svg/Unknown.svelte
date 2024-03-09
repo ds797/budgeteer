5eba1108a88200704299e05d5913b49af22e798d
ds797 <ds797@duck.com>
2024-03-09 01:09:18 -0800

    Add icons

---
+++ src/lib/components/svg/Unknown.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let size = '2rem', fill = 'none', stroke = 'white', color = 'inherit';
+</script>
+
+<main style='width: {size}; height: {size}; fill: {fill}; stroke: {stroke}; color: {color};'>
+	<svg viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<circle cx="64" cy="64" r="59" stroke="currentColor" stroke-width="10" stroke-linecap="round" stroke-dasharray="30 30"/>
+		<circle cx="64" cy="96" r="7" fill="currentColor"/>
+		<path d="M63.2763 78V76.41C63.2763 58.5225 80 63.2925 80 46.995C80 30.6974 54.3026 27.5174 49 43.4174" stroke="currentColor" stroke-width="10" stroke-linecap="round"/>
+	</svg>
+</main>
+
+<style>
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
